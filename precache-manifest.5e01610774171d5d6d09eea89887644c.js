b1e5adcf69a1045f974a3bed502f7f8dbfd44b37
Anik7303 <anik7303@gmail.com>
2020-08-15 13:07:14 +0600

    Updates

---
+++ precache-manifest.5e01610774171d5d6d09eea89887644c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ecae5c1f31a769e976d5dc4e92748c",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "510cf94d8873c930a5de",
+    "url": "/youtube-clone/static/css/main.3964a03e.chunk.css"
+  },
+  {
+    "revision": "0d62c461269321662bae",
+    "url": "/youtube-clone/static/js/2.d2ab8f85.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/youtube-clone/static/js/2.d2ab8f85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "510cf94d8873c930a5de",
+    "url": "/youtube-clone/static/js/main.cd747be8.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  },
+  {
+    "revision": "fb35028c167eed34b70107a56900449c",
+    "url": "/youtube-clone/static/media/youtube.fb35028c.svg"
+  }
+]);
